47f26743dae9943e91cd1e0c47127382b1886d2b
Pablo Penalva <penalva.pablo@gmail.com>
2023-03-21 16:05:48 -0300

    hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        anton: ['Anton', ...defaultTheme.fontFamily.sans],
+        prompt: ['Prompt', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
