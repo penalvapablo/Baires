4a141df2c51ca306c1a5e905987458a1009b8d67
Pablo Penalva <penalva.pablo@gmail.com>
2023-03-22 13:04:17 -0300

    gtm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
+import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
+import partytown from '@astrojs/partytown';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), react()]
+  integrations: [
+    tailwind(),
+    image(),
+    react(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
